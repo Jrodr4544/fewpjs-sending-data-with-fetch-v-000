240f0f67aabdc7a7a894461150d70b39d9fd0fe4
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-06-30 23:20:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+const submitData = (name, email) => {
+  let data = {
+    name: name,
+    email: email
+  }
+  
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+  
+  return fetch("http://localhost:3000/users", configObj)
+            .then(response => {
+              console.log(response)
+              document.getElementsByClass
+            })
+}
